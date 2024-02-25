a2d1104324c8f5e03aea42a0722939a75019365a
Gatanot <nesap123@qq.com>
2024-02-25 13:30:34 +0800

    24.02.25.02

--- test.cpp
+++ test.cpp
@@ -2,8 +2,8 @@
 #include <string.h>
 int main(void)
 {
-char ch1,ch2;
-ch1='A'+'5'-'3';
-ch2='A'+'6'-'3';
-printf("%d,%c\n",ch1,ch2);
+    char ch1, ch2;
+    ch1 = 'A' + '5' - '3';
+    ch2 = 'A' + '6' - '3';
+    printf("%d,%c\n", ch1, ch2);
 }
