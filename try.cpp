c7d1a8ca8e3d0559c3b15f265a95b78141acfc2a
Gatanot <nesap123@qq.com>
2024-03-10 22:57:11 +0800

    24.03.10.01

--- try.cpp
+++ try.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "SortFunctions.cpp"
 using namespace std;
-
-int main()
+int main(void)
 {
-    int number[7] = {231, 423, 1345, 344, 23, 546, 324};
-    vector<int> numlist = {213, 43, 75, 234, 456, 234, 65};
-
-    SORT<int> sortlist;
-    for (int i = 0; i < 7; i++)
-    {
-        cout << number[i] << " ";
-    }
-    cout << endl;
-    sortlist.shell(number, 7);
-    for (int i = 0; i < 7; i++)
-    {
-        cout << number[i] << " ";
-    }
-    cout << endl;
-    for (size_t i = 0; i < 7; i++)
-    {
-        cout << numlist[i] << " ";
-    }
-    cout << endl;
-    sortlist.shell(numlist);
-    for (size_t i = 0; i < 7; i++)
-    {
-        cout << numlist[i] << " ";
-    }
-    cout << endl;
+    int a = 1;
+    const int & const_b = a;
+    cout << const_b << endl;
+    a = 2;
+    cout << const_b << endl;
     return 0;
 }
-
