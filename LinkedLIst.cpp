44957e21e2b3086ce898a236a892ad6de6846ffa
Gatanot <nesap123@qq.com>
2024-04-17 22:30:41 +0800

    24.04.16.03

--- LinkedLIst.cpp
+++ LinkedLIst.cpp
@@ -8,33 +8,24 @@ private:
     Node *next;
 
 public:
-    Node(int newvalue) : value(newvalue), next(nullptr)
-    {
-    }
+    Node(int newvalue) : value(newvalue), next(nullptr) {}
     Node(int newvalue, Node *newnext) : value(newvalue), next(newnext) {}
-    ~Node();
+    ~Node() {}
     int getvalue()
     {
         return value;
     }
-    Node *getnext();
+    Node *getnext()
+    {
+        return next;
+    }
     void setnext(Node *newnext)
     {
         next = newnext;
     }
 };
 
-Node::~Node()
-{
-    ;
-}
-
-Node *Node::getnext()
-{
-    return next;
-}
 // 动态链表
-
 class LinkedList
 {
 private:
@@ -46,60 +37,43 @@ public:
     {
         std::cout << "build right" << std::endl;
     }
-    ~LinkedList();
-    bool isEmpty();
-    int size();
-    void add(int value);
-    void print();
+    ~LinkedList()
+    {
+        delete[] head;
+    }
+    bool isEmpty()
+    {
+        return limit == 0;
+    }
+    int size()
+    {
+        return limit;
+    }
+    void add_front(int value)
+    {
+        std::cout << "add from " << limit;
+        // 创建newnode后为temp的next添加出错
+        head->setnext(new Node(value, head->getnext()));
+        limit++;
+        std::cout << " to " << limit << std::endl;
+    }
+    void print()
+    {
+        Node *temp = head;
+        for (int i = 0; i < limit; i++)
+        {
+            temp = temp->getnext();
+            std::cout << temp->getvalue() << " -> ";
+        }
+    }
 };
-
-LinkedList::~LinkedList()
-{
-    delete[] head;
-}
-
-bool LinkedList::isEmpty()
-{
-    return limit == 0;
-}
-
-int LinkedList::size()
-{
-    return limit;
-}
-
-void LinkedList::add(int value)
-{
-    std::cout << "add from " << limit;
-    Node *temp = head;
-    for (int i = 0; i < limit; i++)
-    {
-        temp = temp->getnext();
-    }
-    Node *newnode = new Node(value, nullptr);
-    // 创建newnode后为temp的next添加出错
-    temp->setnext(newnode);
-    limit++;
-    std::cout << " to " << limit << std::endl;
-}
-
-void LinkedList::print()
-{
-    Node *temp = head;
-    for (int i = 0; i < limit; i++)
-    {
-        temp = temp->getnext();
-        std::cout << temp->getvalue() << " -> ";
-    }
-}
-
 int main()
 {
     LinkedList mylist;
     std::cout << mylist.size() << " " << mylist.isEmpty() << std::endl;
     for (int i = 0; i < 4; i++)
     {
-        mylist.add(i);
+        mylist.add_front(i);
     }
     std::cout << std::endl;
     std::cout << mylist.size() << " " << mylist.isEmpty() << std::endl;
